405264cdd18ebbd0595fcbed4a92cb53fcb7c25e
Jake Soenneker <jake@soenneker.com>
2024-12-21 16:44:30 -0600

    Dep

--- test/Soenneker.Extensions.ParameterInfo.Tests/ParameterInfoExtensionTests.cs
+++ test/Soenneker.Extensions.ParameterInfo.Tests/ParameterInfoExtensionTests.cs
@@ -1,9 +1,8 @@
-using Soenneker.Tests.Unit;
 using Xunit;
 
 namespace Soenneker.Extensions.ParameterInfo.Tests;
 
-public class ParameterInfoExtensionTests : UnitTest
+public class ParameterInfoExtensionTests 
 {
     [Fact]
     public void Default()
