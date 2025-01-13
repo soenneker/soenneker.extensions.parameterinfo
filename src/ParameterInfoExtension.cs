352d764454ed96ef4693d147c26de2b8911bce13
Jake Soenneker <jake@soenneker.com>
2025-01-13 10:56:46 -0600

    Better performance

--- src/ParameterInfoExtension.cs
+++ src/ParameterInfoExtension.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers;
 
 namespace Soenneker.Extensions.ParameterInfo;
 
@@ -8,21 +9,54 @@ namespace Soenneker.Extensions.ParameterInfo;
 public static class ParameterInfoExtension
 {
     /// <summary>
-    /// Converts an array of ParameterInfo objects to an array of their corresponding parameter types.
+    /// Converts an array of <see cref="System.Reflection.ParameterInfo"/> objects into an array of their corresponding <see cref="Type"/> objects.
     /// </summary>
-    /// <param name="parameterInfos">The array of ParameterInfo objects.</param>
-    /// <returns>An array of Type objects representing the parameter types.</returns>
-    public static Type[] ToParametersTypes(this System.Reflection.ParameterInfo[] parameterInfos)
+    /// <param name="parameterInfos">An array of <see cref="System.Reflection.ParameterInfo"/> objects to process.</param>
+    /// <returns>
+    /// An array of <see cref="Type"/> objects representing the parameter types of the provided <paramref name="parameterInfos"/>.
+    /// </returns>
+    /// <remarks>
+    /// This method uses an <see cref="System.Buffers.ArrayPool{T}"/> to minimize memory allocations during processing.
+    /// The array rented from the pool is returned immediately after use to ensure efficient memory management.
+    /// </remarks>
+    /// <example>
+    /// The following example demonstrates how to use the <c>ToTypes</c> method:
+    /// <code>
+    /// var parameterInfos = typeof(SomeClass).GetMethod("SomeMethod")?.GetParameters();
+    /// if (parameterInfos != null)
+    /// {
+    ///     Type[] parameterTypes = parameterInfos.ToTypes();
+    ///     foreach (var type in parameterTypes)
+    ///     {
+    ///         Console.WriteLine(type.FullName);
+    ///     }
+    /// }
+    /// </code>
+    /// </example>
+    public static Type[] ToTypes(this System.Reflection.ParameterInfo[] parameterInfos)
     {
-        ReadOnlySpan<System.Reflection.ParameterInfo> span = parameterInfos;
+        int length = parameterInfos.Length;
 
-        var typesArray = new Type[span.Length];
+        if (length == 0)
+            return [];
 
-        for (var i = 0; i < span.Length; i++)
+        // Rent an array from the pool
+        ArrayPool<Type> arrayPool = ArrayPool<Type>.Shared;
+        Type[] rentedArray = arrayPool.Rent(length);
+
+        // Populate the rented array
+        for (int i = 0; i < length; i++)
         {
-            typesArray[i] = span[i].ParameterType;
+            rentedArray[i] = parameterInfos[i].ParameterType;
         }
 
-        return typesArray;
+        // Create the final array to return
+        var result = new Type[length];
+        Array.Copy(rentedArray, result, length);
+
+        // Return the rented array to the pool immediately
+        arrayPool.Return(rentedArray, clearArray: true);
+
+        return result;
     }
 }
