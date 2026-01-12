9d73ceaaee2e864e5dbd8419ad0ec1830cd42d76
Jake Soenneker <jake@soenneker.com>
2026-01-11 20:27:26 -0600

    readonly imp

--- src/ParameterInfoExtension.cs
+++ src/ParameterInfoExtension.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
+using Soenneker.Extensions.Spans.Readonly.ParameterInfos;
 
 namespace Soenneker.Extensions.ParameterInfo;
 
@@ -16,26 +17,4 @@ public static class ParameterInfoExtension
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Type[] ToTypes(this System.Reflection.ParameterInfo[] parameterInfos) =>
         ((ReadOnlySpan<System.Reflection.ParameterInfo>)parameterInfos).ToTypes();
-
-    /// <summary>
-    /// Converts a span of <see cref="ParameterInfo"/> into an array of their corresponding <see cref="Type"/> objects.
-    /// </summary>
-    [Pure]
-    public static Type[] ToTypes(this ReadOnlySpan<System.Reflection.ParameterInfo> parameterInfos)
-    {
-        int length = parameterInfos.Length;
-
-        if (length == 0)
-            return [];
-
-        if (length == 1)
-            return [parameterInfos[0].ParameterType];
-
-        var result = new Type[length];
-
-        for (var i = 0; i < length; i++)
-            result[i] = parameterInfos[i].ParameterType;
-
-        return result;
-    }
 }
